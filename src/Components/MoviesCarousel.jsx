8465868ed2b9cdb91a688e3b03eb6c6d5753b5d8
Alireza <alirezaalavi7713@gmail.com>
2026-05-12 19:00:06 +0200

    Mobile: center burger menu links, full width carousels on mobile

--- src/Components/MoviesCarousel.jsx
+++ src/Components/MoviesCarousel.jsx
@@ -6,7 +6,7 @@ const MoviesCarousel = ({ title, movies }) => {
     const Navigate = useNavigate();
     return <>
         <h2>{title}</h2>
-        <Carousel className="col-6">
+        <Carousel className="w-100 col-lg-8 mx-auto">
             {movies.map((movies) => {
                 return <Carousel.Item key={movies.id}>
                     <img onClick={() => Navigate('/movie/' + movies.id)} className="block w-100 rounded cursor" src={"https://image.tmdb.org/t/p/original" + movies.backdrop_path}></img>
